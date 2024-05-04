cee4868a9e4a9962a7f64c488204197a2a857495
Saksham Gupta <f20180674@pilani.bits-pilani.ac.in>
2024-05-04 22:04:28 +0530

    link

--- src/components/Fotter.jsx
+++ src/components/Fotter.jsx
@@ -8,7 +8,7 @@ function Fotter() {
                     <a className='px-1 text-blue-700 font-medium' href="https://twitter.com/subhashchy/status/1744308069751025894?t=MrLV-PSnfsgv0Hg2jTz5JA&s=08">x (formely Twitter) post</a>
                 ---
             </div>
-            <div  className='text-black-300 underline' ><a href="https://github.com/Shivam29k/dukaaan-app-ui">GitHub Repo</a></div>
+            <div  className='text-black-300 underline' ><a href="https://github.com/G-Saksham/Frontend-Dukaan-dashboard-clone.git">GitHub Repo</a></div>
         </div>
     )
 }
